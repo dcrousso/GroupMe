07d1d05d6749a3f5ebb8d963f28f42e70a72b033
Devin Rousso <dcrousso@gmail.com>
2016-08-15 10:39:19 -0700

    Create GroupMe app

---
+++ index.js
@@ -0,0 +1,76 @@
+const electron   = require("electron");
+const filesystem = require("fs");
+const path       = require("path");
+
+require("electron-debug")();
+require("electron-dl")();
+
+let mainWindow = null;
+let isQuitting = false;
+
+const isAlreadyRunning = electron.app.makeSingleInstance(() => {
+	if (!mainWindow)
+		return;
+
+	if (mainWindow.isMinimized())
+		mainWindow.restore();
+
+	mainWindow.show();
+});
+
+if (isAlreadyRunning)
+	electron.app.quit();
+
+function createMainWindow() {
+	const browser = new electron.BrowserWindow({
+		width: 900,
+		height: 675,
+		minWidth: 600,
+		minHeight: 450,
+		title: electron.app.getName(),
+		show: false,
+		autoHideMenuBar: true,
+		titleBarStyle: "hidden-inset",
+		webPreferences: {
+			nodeIntegration: false,
+			webSecurity: false
+		}
+	});
+
+	browser.on("close", event => {
+		if (isQuitting)
+			return;
+
+		event.preventDefault();
+		electron.app.hide();
+	});
+
+	browser.loadURL("https://web.groupme.com/signin", {userAgent: ""});
+
+	browser.webContents.on("dom-ready", () => {
+		browser.show();
+	});
+
+	browser.webContents.on("new-window", (event, url) => {
+		event.preventDefault();
+		electron.shell.openExternal(url);
+	});
+
+	return browser;
+}
+
+electron.app.on("ready", () => {
+	if (!mainWindow)
+		mainWindow = createMainWindow();
+});
+
+electron.app.on("activate", () => {
+	if (!mainWindow)
+		mainWindow = createMainWindow();
+
+	mainWindow.show();
+});
+
+electron.app.on("before-quit", () => {
+	isQuitting = true;
+});
---
+++ readme.md
@@ -0,0 +1,32 @@
+# <img src="assets/Icon.png" width="40" align="left">GroupMe
+
+> Unofficial GroupMe app
+
+[![](media/screenshot.png)](https://github.com/dcrousso/GroupMe/releases/latest)
+
+## Download
+
+[Current Release](https://github.com/dcrousso/GroupMe/releases/latest)
+
+## Development
+
+> Made with [Electron](http://electron.atom.io).
+
+<table>
+	<tr>
+		<td>Init</td>
+		<td><code>npm install</code></td>
+	</tr>
+	<tr>
+		<td>Run</td>
+		<td><code>npm start</code></td>
+	</tr>
+	<tr>
+		<td>Pack</td>
+		<td><code>npm run pack</code></td>
+	</tr>
+</table>
+
+## License
+
+MIT © [Devin Rousso](http://devinrousso.com)
